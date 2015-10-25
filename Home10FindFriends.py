cc82c32c3d7be1aff0dedc528002b3451cc85876
s.grudowska <s.grudowska@gmail.com>
2015-10-25 18:27:39 +0100

    Droid connections start

--- Home10FindFriends.py
+++ Home10FindFriends.py
@@ -1,1 +1,30 @@
 __author__ = 'sylwia'
+
+
+def check_connection(network, first, second):
+    set = ()
+
+    for n in network:
+        if first in n:
+            pass
+            # set.add(n - first)
+        print n
+        print n.strip('-')
+
+    return True or False
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert check_connection(
+        ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
+         "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
+        "scout2", "scout3") == True, "Scout Brotherhood"
+    assert check_connection(
+        ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
+         "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
+        "super", "scout2") == True, "Super Scout"
+    assert check_connection(
+        ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
+         "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
+        "dr101", "sscout") == False, "I don't know any scouts."
